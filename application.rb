c1784597eada1a9e0ab4874b9a27c4233cc53085
Lincoln Stoll <lstoll@lstoll.net>
2009-12-01 22:15:05 +1100

    Save tweets to a database to ensure we have them all and a correct count, block RT's without blocking works that contain rt

--- application.rb
+++ application.rb
@@ -3,6 +3,16 @@ require 'sinatra'
 require 'twitter'
 require 'haml'
 require 'pp'
+require 'sequel'
+
+# Database setup
+DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://tfa.db')
+DB.create_table? :tweets do
+  primary_key :id
+  String :content
+  BigNum :twitter_id
+end
+tweets = DB[:tweets]
 
 # reset stylesheet
 get '/stylesheets/reset.css' do
@@ -19,17 +29,42 @@ end
 # homepage
 get '/' do
   if $last_updated + 120 < Time.now # hasn't been updated for 2 mins. - 120 secs
+    # get the max id from the database to pass to our search query
+    since_id = tweets.max(:twitter_id) 
+    page = 1
+
+    # load all the new tweets into the DB
+    while true do
+      @search = Twitter::Search.new('"tony abbott" fuck OR fucking OR fucked OR shit')
+      # 20 per page - twitter docs say 100, but seems to be less, so we
+      # cover our bases for pagination. this pagination method also leaves
+      # a small possibility of duplicates, but it's not a big deal.
+      @search.per_page(20)
+      @search.page(page)
+      @search.since(since_id)
+      item_count = 0
+
+      @search.each do |item|
+        tweets.insert(:twitter_id => item["id"].to_i, :content => item["text"])
+        item_count = item_count + 1
+      end
+      # if we don't have more than 20 items, we can exit
+      break if item_count < 20
+      page = page + 1
+    end
+   
     @results = []
-  
-    @search = Twitter::Search.new('"tony abbott" fuck OR fucking OR fucked OR shit')
-    @search.per_page(100)
-    @search.each do |item|
-      unless BLACKLISTED_STRINGS.any? {|i| item["text"].downcase.match(i.downcase)}
-          @results << item["text"].gsub(/^@\w[a-z]+\s/, '').
-                                    gsub(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/i, '<a href="\1">\1</a>').
-                                    gsub(/(@\w[a-z]+)(\s|\S)/i, '<a href="http://twitter.com/\1">\1</a>').
-                                    gsub(/(Tony Abbott\W?)/i, '<strong>\1</strong>').
-                                    gsub(/(fuck\W|fucking\W|fucked\W|shit\W)/i, '<em>\1</em>')
+
+    @all_results = tweets.order(:twitter_id.desc).each do |item|
+      # ignore items in the blacklist file or that start with rt/RT (retweets)
+      unless BLACKLISTED_STRINGS.any? {|i| item[:content].downcase.match(i.downcase)} || item[:content].downcase.match(/^rt/)
+        @results << item[:content].gsub(/^@\w[a-z]+\s/, '').
+                                  gsub(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/i, '<a href="\1">\1</a>').
+                                  gsub(/(@\w[a-z]+)(\s|\S)/i, '<a href="http://twitter.com/\1">\1</a>').
+                                  gsub(/(Tony Abbott\W?)/i, '<strong>\1</strong>').
+                                  gsub(/(fuck\W|fucking\W|fucked\W|shit\W)/i, '<em>\1</em>')
+      else
+        puts "This was blacklisted: #{item[:content]}"
       end
     end
     
@@ -43,6 +78,9 @@ end
 
 # Configure Block.
 configure do
+  # This cache is incredibly primitive, but it works, and saves us hammering twitter too often
+  # even if we have multiple instances, it's not a big deal - we don't need the cache
+  # synchronized between them.
   Cache = {}      # Create a new cache
   $last_updated = Time.now - 100000 # ensure the first request is outdated.
   BLACKLISTED_STRINGS = []
